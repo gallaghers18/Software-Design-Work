039e385a96a4bbe6e9cb87d2e14dd7495f94202a
gallaghers <gallaghers@cmc102-08.mathcs.carleton.edu>
2018-03-28 20:08:47 -0500

    Added Hello World code

--- Autocomplete/src/edu/carleton/buddgallagher/Main.java
+++ Autocomplete/src/edu/carleton/buddgallagher/Main.java
@@ -3,6 +3,7 @@ package edu.carleton.buddgallagher;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Hello world!");
+
     }
 }
