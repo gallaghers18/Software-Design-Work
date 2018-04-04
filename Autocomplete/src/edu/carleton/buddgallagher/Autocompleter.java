6deb7c93f45e32f986faef5f92013990504b7f75
buddelizabeth <budde@EliBudd-PC.localdomain>
2018-04-04 09:38:03 -0500

    Added code from class on 4/4/2018.

--- Autocomplete/src/edu/carleton/buddgallagher/Autocompleter.java
+++ Autocomplete/src/edu/carleton/buddgallagher/Autocompleter.java
@@ -2,17 +2,22 @@
  * Autocompleter.java
  * Jeff Ondich, 20 March 2018
  *
+ * Sean Gallagher, Elizabeth Budd, Tanvi Mehta
+ *
  * This class exposes a very simple interface for generating auto-completions of search strings.
  * The purpose of this class is to give the students in CS257 an opportunity to practice creating
  * unit tests.
  */
 package edu.carleton.buddgallagher;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Autocompleter {
 
+    private ArrayList<Actor> actors;
     /**
      * @param dataFilePath the path to the data file containing the set of items to
      * from which auto-completed results will be drawn. (In the context of this assignment,
@@ -25,7 +30,22 @@ public class Autocompleter {
         // from a file into a list (or a hashmap or something like that)
         // the list of strings that are going to form the dataset of potential
         // auto-completions. The initialization will be up to you.
+        actors = new ArrayList<Actor>();
+
+        File inputFile = new File(dataFilePath);
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(inputFile);
+        } catch (FileNotFoundException e) {
+            System.err.println(e);
+            return;
+        }
+
+
+
     }
+
+
 
     /**
      * @param searchString the string whose autocompletions are sought
@@ -36,4 +56,17 @@ public class Autocompleter {
     public List<String> getCompletions(String searchString) {
         return new ArrayList<String>();
     }
+}
+
+private class Actor {
+
+    private String displayName;
+    private String searchName;
+    private int order;
+    private int indexMatch;
+
+    private Actor(String displayName) {
+        
+    }
+
 }
--- Autocomplete/src/edu/carleton/buddgallagher/Main.java
+++ Autocomplete/src/edu/carleton/buddgallagher/Main.java
@@ -3,9 +3,6 @@ package edu.carleton.buddgallagher;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Please work");
-        System.out.println("It did.");
 
     }
 }
