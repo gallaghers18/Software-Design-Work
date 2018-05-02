d5ee442f88863dd120033e6fda63a2be8df6794b
gallaghers <gallaghers@cmc102-05.mathcs.carleton.edu>
2018-05-02 17:09:29 -0500

    added stats endpoint

--- webapp/flask-sample.py
+++ webapp/flask-sample.py
@@ -83,12 +83,37 @@ def get_player(player_id):
     
     return json.dumps(player_list)
 
+
 @app.route('/stat/<stat_name>')
 def get_top_25(stat_name):
+     try:
+        connection = psycopg2.connect(database=database, user=user, password=password)
+    except Exception as e:
+        print(e)
+        exit()
     
-    
-    return None
+    try:
+        cursor = connection.cursor()
+        query = 'SELECT * FROM players ORDER BY {0} DESC LIMIT 25'.format(stat_name)
+        cursor.execute(query)
+    except Exception as e:
+        print(e)
+        exit()
 
+    # We have a cursor now. Iterate over its rows to print the results.
+    player_list=[]
+    for row in cursor:
+        player = {}
+        col_num=0
+        for col_name in cursor.description:
+            player[col_name[0]]=row[col_num]
+            col_num+=1
+        player_list.append(player)
+        
+    connection.close()
+    
+    return json.dumps(player_list)
+    
 
 
 if __name__ == '__main__':
