9a419b92a6ce90a153a7513c9325859f3bd7109b
gallaghers <gallaghers@cmc102-15.mathcs.carleton.edu>
2018-05-03 17:43:36 -0500

    updated teams/id endpoint

--- webapp/api.py
+++ webapp/api.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 '''
-    example_flask_app.py
-    Jeff Ondich, 25 April 2018
-    A slightly more complicated Flask sample app than the
-    "hello world" app found at http://flask.pocoo.org/.
+    api.py
+    Sean Gallagher & David White, May 3, 2018
+    Adapted from example_flask_app.py by Jeff Ondich
 '''
 import sys
 import flask
@@ -13,7 +12,6 @@ from config import password
 from config import database
 from config import user
 
-#log into database
 
 
 app = flask.Flask(__name__)
@@ -22,7 +20,60 @@ app = flask.Flask(__name__)
 def hello():
     return 'Go to a more interesting page! (You know the endpoints...)'
 
-@app.route('/teams')
+@app.route('/team/<team_id>')
+def get_team(team_id):
+    ''' Returns a list containing each matching team. Each team is a dictionary with  
+        (I)team stats and (II)a list of each player with their stats'''
+
+    try:
+        connection = psycopg2.connect(database=database, user=user, password=password)
+    except Exception as e:
+        print(e)
+        exit()
+    
+    team_dict = {}
+    
+    try:
+        cursor = connection.cursor()
+        query = 'SELECT * FROM teams WHERE id={0}'.format(team_id)
+        cursor.execute(query)
+    except Exception as e:
+        print(e)
+        exit()
+        
+    team_stats = {}
+    col_num=0
+    for col_name in cursor.description:
+        team[col_name[0]]=row[col_num]
+        col_num+=1
+    team_dict['team_stats'] = team_stats
+    
+    try:
+        cursor = connection.cursor()
+        query = 'SELECT players.* FROM players, teams, player_team WHERE teams.id = {0} AND teams.id = player_team.team_id AND players.id = player_team.player_id AND players.team_code = player_team.team_code;'.format(team_id)
+        cursor.execute(query)
+    except Exception as e:
+        print(e)
+        exit()
+
+    player_list =[]
+    for row in cursor:
+        player = {}
+        col_num=0
+        for col_name in cursor.description:
+            player[col_name[0]]=row[col_num]
+            col_num+=1
+        player_list.append(player)
+        
+    team_dict['player_list'] = player_list
+    
+        
+    connection.close()
+    
+    return json.dumps(team_dict)
+
+
+@app.route('/team')
 def get_all_teams():
     ''' Returns a list of all the teams, each team conisting off some season statistics '''
     try:
@@ -33,46 +84,12 @@ def get_all_teams():
     
     try:
         cursor = connection.cursor()
-        query = 'SELECT * FROM'
+        query = 'SELECT * FROM teams'
         cursor.execute(query)
     except Exception as e:
         print(e)
         exit()
 
-    # We have a cursor now. Iterate over its rows to print the results.
-    team_list=[]
-    for row in cursor:
-        team = {}
-        col_num=0
-        for col_name in cursor.description:
-            team[col_name[0]]=row[col_num]
-            col_num+=1
-        team_list.append(team)
-        
-    connection.close()
-    
-    return json.dumps(team_list)
-
-
-@app.route('/team/<team_id>')
-def get_team(team_id):
-    ''' Returns a list containing each matching team. Each team is a dictionary with  
-        (I)team stats and (II)a list of each player with their stats'''
-    try:
-        connection = psycopg2.connect(database=database, user=user, password=password)
-    except Exception as e:
-        print(e)
-        exit()
-    
-    try:
-        cursor = connection.cursor()
-        query = 'SELECT * FROM teams WHERE id = {0}'.format(team_id)
-        cursor.execute(query)
-    except Exception as e:
-        print(e)
-        exit()
-
-    # We have a cursor now. Iterate over its rows to print the results.
     team_list=[]
     for row in cursor:
         team = {}
@@ -104,7 +121,6 @@ def get_player(player_id):
         print(e)
         exit()
 
-    # We have a cursor now. Iterate over its rows to print the results.
     player_list=[]
     for row in cursor:
         player = {}
@@ -135,7 +151,6 @@ def get_top_25(stat_name):
         print(e)
         exit()
 
-    # We have a cursor now. Iterate over its rows to print the results.
     player_list=[]
     for row in cursor:
         player = {}
