3f1455a80aac1facf12e3211b86ed49355111e74
Gallaghers18 <gallaghers@carleton.edu>
2018-06-02 13:31:50 -0500

    Fixed stoplight and car views

--- final/src/traffic/TrafficView.java
+++ final/src/traffic/TrafficView.java
@@ -5,11 +5,13 @@ import javafx.fxml.FXML;
 import javafx.scene.Group;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Slider;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
+
 
 
 public class TrafficView extends Group {
@@ -26,49 +28,78 @@ public class TrafficView extends Group {
         this.getChildren().add(canvas);
 
         //Vertical lines
-        for (int x=1; x<NUM_ROADS+1; x++) {
-            for (int i=-1; i<2; i++) {
-                gc.strokeLine(START_X+(ROAD_LENGTH*x+8*i), START_Y,   START_X+(ROAD_LENGTH*x+8*i),   START_Y+((NUM_ROADS+1)*ROAD_LENGTH));
+        for (int x = 1; x < NUM_ROADS + 1; x++) {
+            for (int i = -1; i < 2; i++) {
+                gc.strokeLine(START_X + (ROAD_LENGTH * x + 8 * i), START_Y, START_X + (ROAD_LENGTH * x + 8 * i), START_Y + ((NUM_ROADS + 1) * ROAD_LENGTH));
             }
 
         }
         //Horizontal lines
-        for (int y=1; y<NUM_ROADS+1; y++) {
-            for (int i=-1; i<2; i++) {
-                gc.strokeLine(START_X,START_Y+(ROAD_LENGTH*y+8*i), START_X+((NUM_ROADS+1)*ROAD_LENGTH), START_Y+(ROAD_LENGTH*y+8*i));
+        for (int y = 1; y < NUM_ROADS + 1; y++) {
+            for (int i = -1; i < 2; i++) {
+                gc.strokeLine(START_X, START_Y + (ROAD_LENGTH * y + 8 * i), START_X + ((NUM_ROADS + 1) * ROAD_LENGTH), START_Y + (ROAD_LENGTH * y + 8 * i));
             }
         }
 
 
-        for (int x=1; x<NUM_ROADS+1; x++) {
-            for (int y=1; y<NUM_ROADS+1; y++) {
-                drawStoplight(false, x, y);
+        for (int x = 1; x < NUM_ROADS + 1; x++) {
+            for (int y = 1; y < NUM_ROADS + 1; y++) {
+                drawStoplight(true, x, y);
             }
         }
+
+        //EXAMPLE OF A CAR. REQUIRES MUCH MORE AUTOMATED LOCATION FINDING. NEED STRUCTURE OF HOW ROADS ARE SAVED.
+        for (int i = 0; i<13; i++) {
+            drawCar(START_X + ROAD_LENGTH * 1 - 7, START_Y + ROAD_LENGTH * 1 - 16 - 8 * i);
+        }
+
+        for (int i = 0; i<13; i++) {
+            drawCar(START_X + ROAD_LENGTH * 3- 16 - 8 * i, START_Y + ROAD_LENGTH * 2 + 1);
+        }
+
+        createSlider();
 
         //THIS IS JUST TEMPORARY SO I CAN VISUALLY SEE WHERE THE CANVAS IS AS WINDOW RESHAPES.
-        gc.setFill(Color.rgb(0, 255, 0, 0.2));
-        gc.fillRect(0,0,700,700);
+//        gc.setFill(Color.rgb(0, 255, 0, 0.2));
+//        gc.fillRect(0,0,700,700);
 
 
     }
 
     public void drawRoadSegment(RoadSegment road) {
-        Line line = new Line(road.getIn().getXpos(), road.getIn().getYpos(), road.getOut().getXpos(), road.getOut().getYpos());
+       //take RoadSegment object and drawCar() based on each car in it.
+//        for () {
+//            if (car != 0) {
+//                drawCar(5, 5); //PLACEHOLDER
+//            }
+//        }
+
+    }
+
+    public void drawCar(int x, int y) {
+        gc.setFill(Color.GREEN);
+        gc.clearRect(x, y, 6, 6);
+        gc.fillRect(x, y, 6, 6);
+        gc.setFill(Color.BLACK);
 
     }
 
 
-    public void drawStoplight(boolean north_south, int x, int y) {
+        public void drawStoplight(boolean open_north_south, int x, int y) {
         gc.setFill(Color.BLACK);
         gc.clearRect(START_X+(ROAD_LENGTH*x-7),START_Y+(ROAD_LENGTH*y-7), 14, 14);
-        if (north_south) {
-            gc.fillRect(START_X+(ROAD_LENGTH*x-6),START_Y+(ROAD_LENGTH*y-8), 4, 16);
-            gc.fillRect(START_X+(ROAD_LENGTH*x+2),START_Y+(ROAD_LENGTH*y-8), 4, 16);
-
+        if (open_north_south) {
+            gc.fillPolygon(new double[]{START_X+ROAD_LENGTH*x-6, START_X+ROAD_LENGTH*x, START_X+ROAD_LENGTH*x+6},
+                    new double[]{START_Y+ROAD_LENGTH*y+4, START_Y+ROAD_LENGTH*y+8, START_Y+ROAD_LENGTH*y+4}, 3);
+            gc.fillPolygon(new double[]{START_X+ROAD_LENGTH*x-6, START_X+ROAD_LENGTH*x, START_X+ROAD_LENGTH*x+6},
+                    new double[]{START_Y+ROAD_LENGTH*y-4, START_Y+ROAD_LENGTH*y-8, START_Y+ROAD_LENGTH*y-4}, 3);
+            gc.fillRect(START_X+ROAD_LENGTH*x-2,START_Y+ROAD_LENGTH*y-4, 4, 8);
         } else {
-            gc.fillRect(START_X+(ROAD_LENGTH*x-8),START_Y+(ROAD_LENGTH*8-6), 16, 4);
-            gc.fillRect(START_X+(ROAD_LENGTH*x-8),START_Y+(ROAD_LENGTH*8+2), 16, 4);
+            gc.fillPolygon(new double[]{START_X+ROAD_LENGTH*x+4, START_X+ROAD_LENGTH*x+8, START_X+ROAD_LENGTH*x+4},
+                    new double[]{START_Y+ROAD_LENGTH*y-6, START_Y+ROAD_LENGTH*y, START_Y+ROAD_LENGTH*y+6}, 3);
+            gc.fillPolygon(new double[]{START_X+ROAD_LENGTH*x-4, START_X+ROAD_LENGTH*x-8, START_X+ROAD_LENGTH*x-4},
+                    new double[]{START_Y+ROAD_LENGTH*y-6, START_Y+ROAD_LENGTH*y, START_Y+ROAD_LENGTH*y+6}, 3);
+            gc.fillRect(START_X+ROAD_LENGTH*x-4,START_Y+ROAD_LENGTH*y-2, 8, 4);
         }
     }
 
@@ -81,7 +112,16 @@ public class TrafficView extends Group {
         //SOME LOOPS TO DRAW NODES, DRAW CARS, ETC (I need to know how they will be stored to do this).
     }
 
-    public void drawCar(int x, int y) {
 
+    //THIS WILL ACTUALLY LIVE IN THE CONTROLLER SORT OF, BUT JUST HERE SO I REMEMBER THE CODE
+    public void createSlider() {
+        Slider slider = new Slider(0, 100,50);
+        slider.setShowTickLabels(true);
+        slider.setShowTickMarks(true);
+        slider.setMajorTickUnit(50);
+        slider.setMinorTickCount(5);
+        slider.setBlockIncrement(10);
+        this.getChildren().add(slider);
     }
+
 }
